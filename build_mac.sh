31e23be45f76b3df2d340f3a83902b523a5b3bce
Dmitry Konovalov <dkanavis@gmail.com>
2021-09-17 18:25:13 +0300

    fix name

--- build_mac.sh
+++ build_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-env/bin/pyinstaller --osx-bundle-identifier 'name' --icon 'ktlauncher/res/app_icon.icns' --windowed --add-data "./ktlauncher/res/lucky.png:./ktlauncher/res/" main.py
+env/bin/pyinstaller --osx-bundle-identifier 'Lucky Launcher' --icon 'ktlauncher/res/app_icon.icns' --windowed --add-data "./ktlauncher/res/lucky.png:./ktlauncher/res/" main.py
